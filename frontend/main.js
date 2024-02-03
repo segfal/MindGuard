f1ed51fd56079e056aec7b8c7d967a50bf7aa98c
alexakfk <alexakfk@gmail.com>
2024-02-03 12:29:07 -0500

    logo atribution

---
+++ frontend/main.js
@@ -0,0 +1,2 @@
+// icon atribution (make sure to uncomment)
+// <a href="https://www.flaticon.com/free-icons/mental-health" title="mental health icons">Mental health icons created by juicy_fish - Flaticon</a> 
