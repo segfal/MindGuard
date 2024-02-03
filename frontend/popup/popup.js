7195410f5c2f2e2f44a1fe97478c2b5b9b731a14
segfaultboi@gmail.com <segfaultboi@gmail.com>
2024-02-03 14:55:56 -0500

    added permission for popup and extension to use tab

--- frontend/popup/popup.js
+++ frontend/popup/popup.js
@@ -1,1 +1,22 @@
-console.log('This is a pop-up!');
+chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+    var currentURL = tabs[0].url;
+    console.log(currentURL);
+    if (currentURL.includes('leetcode.com')) {
+        var url = new URL(currentURL);
+        var pathname = url.pathname;
+        var parts = pathname.split('/');
+        var questionName = parts[parts.length - 2];
+
+
+        try{
+            psuedoCode
+        }
+        catch(error){
+            console.log("error", error);
+        }
+        
+    } else 
+        console.log('This is not a leetcode website.');
+}
+);
+
